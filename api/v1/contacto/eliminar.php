7e811670389b801c8dabe3033916c52200419741
ismaelfrica <ismaelfrica@hotmail.com>
2022-04-07 23:54:13 -0400

    updtae parameters

--- api/v1/contacto/eliminar.php
+++ api/v1/contacto/eliminar.php
@@ -1,7 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: PUT");
 
 
 include("../../../libreria/engine.php");
@@ -12,6 +12,7 @@ $data = json_decode(file_get_contents('php://input'), true);
 //instancio la clase
 $contacto = new atl_contacto();
 
+//mensaje
 
-//validacion y ejecucion de agregar
-validarRegistro($data,'agregar');
+//validacion de agregar
+validarRegistro($data,'eliminar');
--- api/v1/contacto/getcontacto.php
+++ api/v1/contacto/getcontacto.php
@@ -1,7 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: GET");
 
 
 include("../../../libreria/engine.php");
@@ -12,6 +12,5 @@ $data = json_decode(file_get_contents('php://input'), true);
 //instancio la clase
 $contacto = new atl_contacto();
 
-
-//validacion y ejecucion de agregar
-validarRegistro($data,'agregar');
+//muestro listado
+$contacto->listarContactos();
